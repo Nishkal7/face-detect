4833d3df0a73e06c2922cb0a6acc46bd0fe07cb5
Nishkal <nishkal8463@gmail.com>
2019-10-15 22:56:23 +0530

    adding

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ class App extends Component{
   }
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-       fetch('http://localhost:3000/imageurl', {
+       fetch('https://evening-basin-55717.herokuapp.com/imageurl', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
@@ -86,7 +86,7 @@ class App extends Component{
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://evening-basin-55717.herokuapp.com:3000/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
